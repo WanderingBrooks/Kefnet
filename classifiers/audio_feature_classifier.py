b1cc3a586536f807a08a35b5d69d61bb93601818
Jason Brooks <cs.jasonbrooks@gmail.com>
2020-04-12 20:29:50 -0400

    Combining features

--- classifiers/audio_feature_classifier.py
+++ classifiers/audio_feature_classifier.py
@@ -1,8 +1,8 @@
 import json
+from scipy.sparse import coo_matrix, hstack
 from sklearn import preprocessing, svm, metrics
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 
 def get_labels_and_features(data):
@@ -20,62 +20,78 @@ def get_labels_and_features(data):
   return labels, audio_features, genre_types
 
 def get_labels_and_lyrics(data):
-  labels = []
   lyrics = []
 
   for track in data:
     if isinstance(track['Lyrics'], str):
       lyrics.append(track['Lyrics'])
-      labels.append(track['Genre'])
-  return labels, lyrics
 
-f = open("../spotify-data-fetcher/audio-features.json", "r")
-#
-audio_features = json.loads(f.read())
-#
-labels, audio_features, genre_types = get_labels_and_features(audio_features)
-#
-# # Encode genres
-# le = preprocessing.LabelEncoder()
-#
-# genres_encoded = le.fit_transform(labels)
-#
-# # Split Data
-# X_train, X_test, y_train, y_test = train_test_split(audio_features, genres_encoded, test_size=0.3)
-#
-# # Train
-# clf = svm.SVC(decision_function_shape='ovo') # One vs One Multi-class
-#
-# clf.fit(X_train, y_train)
-#
-# # Predict
-# y_pred = clf.predict(X_test)
-#
-# # Get stats
-# print(genre_types)
-# print(metrics.classification_report(y_test, y_pred))
+  return lyrics
 
 b = open("../genius-scraper/bag_of_words.json", "r")
 bag_of_words_data = json.loads(b.read())
-labels, lyrics = get_labels_and_lyrics(bag_of_words_data)
+print(len(bag_of_words_data))
+lyrics = get_labels_and_lyrics(bag_of_words_data)
 
 count_vect = CountVectorizer()
 tfidf_transformer = TfidfTransformer()
-X_train_counts = count_vect.fit_transform(lyrics)
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+counts = count_vect.fit_transform(lyrics)
+tfidf = tfidf_transformer.fit_transform(counts)
+
+f = open("../spotify-data-fetcher/audio-features.json", "r")
+
+audio_features = json.loads(f.read())
+
+print(len(audio_features))
+
+labels, audio_features, genre_types = get_labels_and_features(audio_features)
+
+#print(len(audio_features), len(lyrics))
+
+features = hstack([tfidf, coo_matrix(audio_features)])
 
 # Encode genres
 le = preprocessing.LabelEncoder()
 
 genres_encoded = le.fit_transform(labels)
-X_train, X_test, y_train, y_test = train_test_split(X_train_tfidf, genres_encoded, test_size=0.3)
 
-clf = MultinomialNB()
-clf.fit(X_train, y_train)
+# Split Data
+X_train, X_test, y_train, y_test = train_test_split(features, genres_encoded, test_size=0.3)
+
+# Train
+svm_CLF = svm.SVC(decision_function_shape='ovo') # One vs One Multi-class
+
+svm_CLF.fit(X_train, y_train)
 
 # Predict
-y_pred = clf.predict(X_test)
+y_pred = svm_CLF.predict(X_test)
 
 # Get stats
 print(genre_types)
 print(metrics.classification_report(y_test, y_pred))
+
+# b = open("../genius-scraper/bag_of_words.json", "r")
+# bag_of_words_data = json.loads(b.read())
+# labels, lyrics = get_labels_and_lyrics(bag_of_words_data)
+
+# count_vect = CountVectorizer()
+# tfidf_transformer = TfidfTransformer()
+# X_train_counts = count_vect.fit_transform(lyrics)
+# X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+# # Encode genres
+# le = preprocessing.LabelEncoder()
+
+# genres_encoded = le.fit_transform(labels)
+# X_train, X_test, y_train, y_test = train_test_split(X_train_tfidf, genres_encoded, test_size=0.3)
+
+# clf = MultinomialNB()
+# clf.fit(X_train, y_train)
+
+# # Predict
+# y_pred = clf.predict(X_test)
+
+# # Get stats
+# print(genre_types)
+# print(metrics.classification_report(y_test, y_pred))
+
--- spotify-data-fetcher/index.js
+++ spotify-data-fetcher/index.js
@@ -7,6 +7,80 @@ const GENRES_TO_USE = [
   'rock'
 ];
 
+const SONGS_TO_SKIP = [
+  '2kJwzbxV2ppxnQoYw4GLBZ',
+  '4cI2rd2D44mBjwUVFxTkUZ',
+  '5bl2pNsPhMT37RkDUa57Ab',
+  '78Whqvk23AqxailSEMv83F',
+  '2MqaQfeuanwWhfrJAK8P7K',
+  '5Kjz86OFAoN6D4kF1BTSEc',
+  '1AzqpMy3yLYNITSOUrnL8i',
+  '015GITSMhfWPGvDZTcmwbo',
+  '4gF5IWlKhmVBWzjXFZ5VlW',
+  '2rB3bbEiGVPkL2mxlzsLSc',
+  '6r5vCcdUfYVqkxJzp9Nr1z',
+  '5DYD4zlGiFlkpLaf2Bk8Vl',
+  '39O0nR2jufNAGnZCARLd5S',
+  '6sGTEdVdQ1elMrhg9dS2Eg',
+  '1piXWeTvZCGO8GAvJHYUPc',
+  '4B0r8wQP0qfyDFaPs4Rjnz',
+  '7oOOI85fVQvVnK5ynNMdW7',
+  '0hSzixwdKTfDQewDKq2cP5',
+  '3JTjLyrnevl9ASw3ayGO2P',
+  '1595LW73XBxkRk2ciQOHfr',
+  '58IHCvoCPfMNS0WrK7JnSg',
+  '07rmSXN6vNoquX1AsWd9pP',
+  '1YR9iNGXtsbrd8deR9ULDD',
+  '6URlKrAIlJJwHnHxxXWywt',
+  '0TK2YIli7K1leLovkQiNik',
+  '2adTfABiJJQpZWlujYO3Qo',
+  '0ICY4TtDdjDd0kSR3vv148',
+  '1jXAprN4NTcfleztufIHom',
+  '0P6AWOA4LG1XOctzaVu5tt',
+  '6JqYhSdTE4WbQrMXxPH5cD',
+  '7CTWqCb7LpbwCYawPGg9MZ',
+  '4sjiIpEv617LDXaidKioOI',
+  '0D1pEisM3QkiacGXJe5dmd',
+  '6qVPeiDQyjLbZKN24ufCu7',
+  '5sGb7vB83MhpdZsx1tEIYH',
+  '6vN77lE9LK6HP2DewaN6HZ',
+  '57disvXYFrIA66G27Aa5yG',
+  '6kylq8X8cxbGs4YoeLxgk6',
+  '3UnYMo1aeD0o8VtHy3R8cs',
+  '4DdKpYcyTqDpVOiJuT894E',
+  '1R8kvV2AgNPCA2Pp4Im1Ao',
+  '1nQZC5d0lR56oKRbNLyP8v',
+  '6Qb16QNat979ujJka6hhfQ',
+  '1bBYR801JoHJ6fOlXdmgJa',
+  '5pfJsMwoRYKampPay8amX0',
+  '455AfCsOhhLPRc68sE01D8',
+  '0vRa1EzwXx7b56uXsuctFU',
+  '2Wq9wjMTn2qD1zlfRYJtv8',
+  '6GfTYfb5eBLN1p1ByOsPmi',
+  '4Yxc55NX3tAXC2mHRAhtcW',
+  '4rCKRVJZKVysScn2piDuOT',
+  '0g21KZ1XJuhwexWPLpuEt1',
+  '7BYppcUTuG5ysmaJlGSt3t',
+  '4SDgTLDYrJ2UrHbkRkg7MD',
+  '5MPPttjfGap2C6j6eKcO6J',
+  '4a6q8CR2hzLk2plDkSxkfD',
+  '6cXfnv6CBEr5zWQaQGQLVB',
+  '7k6IzwMGpxnRghE7YosnXT',
+  '7kl7cL0bOFxnOMruVLh8ve',
+  '11LmqTE2naFULdEP94AUBa',
+  '47StfXJOnGLhYfMEWgoTiX',
+  '3hTpoe9foSCDu6NS0vlHQS',
+  '0UrWr7Jnu1heq1o99ZwUd0',
+  '1FTSo4v6BOZH9QxKc3MbVM',
+  '0fIC59ZyS8ThtUF0Um6zuD',
+  '0Xq7FPQaLJgIHSB605YUMy',
+  '51c94ac31swyDQj9B3Lzs3',
+  '2Df8M8sO1zhC71p2XQRwpj',
+  '4PgJ0NUYaDDh659TW5mWBK',
+  '2GiJYvgVaD2HtM8GqD9EgQ',
+  '1hWQvA6oGVJ2mAVsZ59AaV'
+];
+
 const PLAYLIST_IDS = [
   // Easy to classify
   '37i9dQZF1DXcBWIGoYBM5M', // Todays top hits
@@ -47,7 +121,7 @@ const fetch = async() => {
 
       tracks.push(
         ...items.reduce( ( reduced, { track } ) => {
-          if ( track && !seenSongs[ track.id ]  ) {
+          if ( track && !seenSongs[ track.id ] && !SONGS_TO_SKIP.includes(track.id)  ) {
             seenSongs[ track.id ] = true;
             reduced.push({
               trackID: track.id,
